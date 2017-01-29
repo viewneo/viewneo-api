da9edee9b9629b481b137bc9f548beb1d5b3636e
Niels Cordes <niels.cordes@adversign-media.de>
2017-01-29 14:19:25 +0100

    Added Upload Image Example

---
+++ examples/README.md
@@ -0,0 +1,9 @@
+#Examples
+
+##NodeJS
+- [Installation](#installation)
+- [Create Website](./nodejs/create-website.jsx)
+- [Upload Image](./nodejs/upload-image.jsx)
+
+###Installation
+Clone or download the repository and go to [nodejs](./nodejs) directory. Then run `npm install`.
--- examples/nodejs/upload-image.jsx
+++ examples/nodejs/upload-image.jsx
@@ -1,21 +1,21 @@
 var request = require('request');
+var fs = require('fs');
 
 // Your personal access token (see README.md for more information)
 var access_token = 'YOUR ACCESS TOKEN';
 
-// options for the api request including the json body
+// options for the api request including form data
 var options = {
     url: 'https://cloud.viewneo.com/api/v1.0/mediafile',
     method: 'POST',
     headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'multipart/form-data',
         'Authorization': 'Bearer ' + access_token
     },
-    body: JSON.stringify({
-        name: 'viewneo',
+    formData: {
         media_file_id_as_parent_directory: 0,
-        url: 'https://www.viewneo.com'
-    })
+        file: fs.createReadStream('./src/freya.jpg')
+    }
 };
 
 // perform the request and log the response
